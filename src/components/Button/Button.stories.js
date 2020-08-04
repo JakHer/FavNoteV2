2caeb989f4c4871391267fd311dfd6a815d84630
Kuba <kubahermyt@gmail.com>
2020-08-04 09:47:32 +0200

    feat: storybook global styles configured

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,6 +3,8 @@ import Button from 'components/Button/Button';
 
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Button', module).add('Main', () => (
-  <Button>Welcome</Button>
-));
+storiesOf('Button', module)
+  .add('Main', () => <Button>Main</Button>)
+  .add('Secondary', () => (
+    <Button secondary>Secondary</Button>
+  ));
